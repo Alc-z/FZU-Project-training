90de909a7ff3d7236f62c1686ac6b95301691bb4
JustEnvyMe <wzhiwei@linewell.com>
2020-10-14 18:30:44 +0800

    skip button ; login button; logout button

--- src/app/pages/welcome/welcome.page.ts
+++ src/app/pages/welcome/welcome.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ViewEncapsulation } from '@angular/core';
+import { IonSlide, IonSlides } from '@ionic/angular';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class WelcomePage implements OnInit {
+    showSkip = true;
+    @ViewChild('slides', {static: false}) slides: IonSlides;
+    constructor() { }
 
-  constructor() { }
+    ngOnInit() {
+    }
 
-  ngOnInit() {
-  }
-
+    onSlideWillChange(event){
+        console.log(event);
+        this.slides.isEnd().then((end) => {
+            this.showSkip = !end;
+        });
+    }
 }
